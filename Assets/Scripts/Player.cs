8edd1d58380df0bb2a9569594f61b20eeafca72b
Reza Nasution <rezanasution82@gmail.com>
2019-11-17 15:27:09 +0100

    Project Translation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In ms^-1")][SerializeField] float speed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+       
+    }
+
+    private void ProcessTranslation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(
+            clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
